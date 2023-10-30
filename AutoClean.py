127e9cf8bcc02ee4c25b0d37f35f69ff7f504b98
Steven Arne <124228626+burnedfrenchfry@users.noreply.github.com>
2023-10-30 17:44:50 -0400

    Add files via upload

---
+++ AutoClean.py
@@ -0,0 +1,28 @@
+import os
+import datetime
+import send2trash
+
+def moving_old_files(folder_path, months_threshold):
+    #Current Date & Time
+    current_datetime = datetime.datetime.now()
+
+    #Calculate Date Threshold
+    threshold_date = current_datetime - datetime.timedelta(days=months_threshold * 30) #30 Days A Month
+
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+
+        #Check if the file is old
+        if os.path.isfile(file_path) and os.path.getmtime(file_path) < threshold_date.timestamp():
+            #Move the files to recycling bin
+            send2trash.send2trash(file_path)
+            print(f"Moved '{filename}' to recycling bin.")
+
+#Path to cache folder
+cache_folder_path = r'C:\Users\arnes\AppData\Local\Temp'
+
+#Timeframe
+months_threshold = 1
+
+#Call function to move to recycling bin
+moving_old_files(cache_folder_path, months_threshold)
